d031230caa87e50a86845064aadd15f9d6b7617c
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-04 21:52:19 +0900

    Delete src/main/reservation directory

--- src/main/reservation/repository/PlaceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dbcafe.domain.reservation.repository;
-
-import com.example.dbcafe.domain.reservation.domain.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Integer> {
-    Place findPlaceById(int placeId);
-}
--- src/main/reservation/repository/ReservationChangeRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dbcafe.domain.reservation.repository;
-
-import com.example.dbcafe.domain.reservation.domain.ReservationChangeRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationChangeRequestRepository extends JpaRepository<ReservationChangeRequest, Integer> {
-}
