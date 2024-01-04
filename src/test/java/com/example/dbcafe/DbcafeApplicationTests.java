173df5a9bf5cc679bb3dcbc657c6e2ade3b93ee7
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-04 21:36:41 +0900

    Add files via upload

---
+++ src/test/java/com/example/dbcafe/DbcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
